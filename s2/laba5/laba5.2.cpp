a20a12dcf0050c957c327c7bd11a0fb98aea54d9
Danila <thecat7364@gmail.com>
2023-04-15 18:25:29 +0400

    labs

---
+++ s2/laba5/laba5.2.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+#include <random>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<> dist(-100, 100);
+	int size = 0;
+	cin >> size;
+	cout << endl;
+	vector<int> arr;
+	for (int i = 0; i < size; i++) {
+		int x = dist(gen);
+		arr.push_back(x);
+	}
+	for (int i = 0; i < size; i++) {
+		cout << arr[i]<<endl;
+	}
+
+	int n = 0, k = 0;
+	cin >> n >> k;
+	cout << endl;
+	arr.erase(arr.begin() + k, arr.begin() + k + n);
+
+	for (int i = 0; i < arr.size(); i++) {
+		cout << arr[i] << endl;
+	}
+	
+	system("pause");
+	return 0;
+}
